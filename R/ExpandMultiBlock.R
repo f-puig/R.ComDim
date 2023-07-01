5abf10164c6d1fb9458456a1f1c13301ca5b5cdc
f-puig <30553393+f-puig@users.noreply.github.com>
2023-07-01 19:10:53 +0200

    Add files via upload

--- R/ExpandMultiBlock.R
+++ R/ExpandMultiBlock.R
@@ -17,7 +17,7 @@ ExpandMultiBlock <- function(data = NULL, metadata = NULL, minblock = 0, loquace
     stop('No data was provided.')
   } else {
     if(any(duplicated(rownames(data)))){
-      stop('There are duplicated rownames. Data must have variables in rows and samples in columns.')
+      stop('There are duplicated rownames. Data must have samples in rows and variables in columns.')
     }
   }
 
@@ -25,39 +25,43 @@ ExpandMultiBlock <- function(data = NULL, metadata = NULL, minblock = 0, loquace
     stop('No metadata was provided.')
   }
 
-  if(any(!(rownames(data) %in% metadata[,2]))){
+  if(is.numeric(metadata[,2])){
+    metadata[,2] <- as.character(metadata[,2])
+  }
 
-    aa <- rownames(data)[!(rownames(data) %in% metadata[,2])]
+  if(any(!(colnames(data) %in% metadata[,2]))){
+
+    aa <- colnames(data)[!(colnames(data) %in% metadata[,2])]
     if(loquace){
       warning(sprintf('%s row names are not present in metadata: %s',
                       length(aa),
                       paste0(aa, collapse = ", ")))
     }
 
-    data <- data[rownames(data) %in% metadata[,2],]
+    data <- data[,colnames(data) %in% metadata[,2]]
   }
 
-  if(any(!(metadata[,2] %in% rownames(data)))){
-    metadata <- metadata[ metadata[,2] %in% rownames(data),]
+  if(any(!(metadata[,2] %in% colnames(data)))){
+    metadata <- metadata[ metadata[,2] %in% colnames(data),]
   }
 
   if(minblock != 0){
     xx <- as.data.frame(table(metadata[,1]))
     xx <- xx[xx$Freq >= minblock,]
     metadata <- metadata[metadata[,1] %in% xx$Var1,]
-    data <- data[rownames(data) %in% metadata[,2],]
+    data <- data[,colnames(data) %in% metadata[,2]]
   }
 
-  expanded_data <- matrix(ncol = nrow(metadata), nrow = ncol(data))
+  expanded_data <- matrix(ncol = nrow(metadata), nrow = nrow(data))
 
-  for(i in 1:nrow(data)){
-    pos <- which(metadata[,2] == rownames(data)[i])
-    expanded_data[,pos] <- kronecker(matrix(1,1,length(pos)),as.matrix(data[i,]))
+  for(i in 1:ncol(data)){
+    pos <- which(metadata[,2] == colnames(data)[i])
+    expanded_data[,pos] <- kronecker(matrix(1,1,length(pos)),as.matrix(data[,i]))
     # Similar function to repmat from base R
   }
 
   colnames(expanded_data) <- paste(metadata[,1], metadata[,2], sep = ".")
-  rownames(expanded_data) <- colnames(data)
+  rownames(expanded_data) <- rownames(data)
 
   # Remove rows with all NAs
   pos_na <- which(apply(expanded_data, 2, function(x) all(is.na(x))))
